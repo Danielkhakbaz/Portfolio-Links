9d482c928e2919c82e733a2ae377a4d0668c94dc
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-02-25 15:00:21 +0330

    (performance): Add static images instead of the cdn images

--- constants/app/social-media-links.ts
+++ constants/app/social-media-links.ts
@@ -1,53 +1,67 @@
+import { StaticImageData } from "next/image";
+import TelegramLogo from "public/logos/telegram-logo.svg";
+import TelegramIcon from "public/logos/telegram-icon.svg";
+import TwitterLogo from "public/logos/twitter-logo.svg";
+import TwitterIcon from "public/logos/twitter-icon.svg";
+import InstagramLogo from "public/logos/instagram-logo.svg";
+import InstagramIcon from "public/logos/instagram-icon.svg";
+import GithubLogo from "public/logos/github-logo.svg";
+import LinkedinLogo from "public/logos/linkedin-logo.svg";
+import SkypeLogo from "public/logos/skype-logo.svg";
+import SpotifyLogo from "public/logos/spotify-logo.svg";
+import SpotifyIcon from "public/logos/spotify-icon.svg";
+import DiscordLogo from "public/logos/discord-logo.svg";
+
 type socialMediaLinksType = {
   title: string;
   link: string;
-  logo: string;
-  icon?: string;
+  logo: StaticImageData;
+  icon?: StaticImageData;
 };
 
 export const socialMediaLinks: socialMediaLinksType[] = [
   {
     title: "Telegram",
     link: "https://t.me/danielkhakbaz",
-    logo: "https://cdn.solo.to/images/link/telegram.svg",
-    icon: "https://cdn.solo.to/images/social/telegram-min.svg",
+    logo: TelegramLogo,
+    icon: TelegramIcon,
   },
   {
     title: "Twitter",
     link: "https://twitter.com/danielkhakbaz",
-    logo: "https://cdn.solo.to/images/link/twitter.svg",
-    icon: "https://cdn.solo.to/images/social/twitter-min.svg",
+    logo: TwitterLogo,
+    icon: TwitterIcon,
   },
   {
     title: "Instagram",
     link: "https://www.instagram.com/danielkhakbaz/",
-    logo: "https://cdn.solo.to/images/link/instagram.svg",
-    icon: "https://cdn.solo.to/images/social/instagram-min.svg",
+    logo: InstagramLogo,
+    icon: InstagramIcon,
   },
   {
     title: "Github",
     link: "https://github.com/danielkhakbaz",
-    logo: "https://cdn.solo.to/images/link/github.svg",
+    logo: GithubLogo,
   },
   {
     title: "LinkedIn",
     link: "https://www.linkedin.com/in/danielkhakbaz/",
-    logo: "https://cdn.solo.to/images/link/linkedin.svg",
+    logo: LinkedinLogo,
   },
   {
     title: "Skype",
     link: "https://join.skype.com/invite/cC3x8Gzfx1ao",
-    logo: "https://cdn.solo.to/images/link/skype.svg",
+    logo: SkypeLogo,
   },
   {
     title: "Spotify",
     link: "https://open.spotify.com/user/3p0q2i0j7g4e0vo9hc0kkpmov?si=22691cd459ad4736&nd=1",
-    logo: "https://cdn.solo.to/images/link/spotify.svg",
-    icon: "https://cdn.solo.to/images/social/spotify-min.svg",
+    logo: SpotifyLogo,
+    icon: SpotifyIcon,
   },
   {
     title: "Discord",
     link: "https://www.discordapp.com/users/723483973409570816",
-    logo: "https://cdn.solo.to/images/link/discord.svg",
+    logo: DiscordLogo,
   },
 ];
